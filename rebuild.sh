55abdeb66855b6adbdedc4bd3232f0431a4a55e7
retorquere <Emiliano.Heyns@iris-advies.com>
2016-11-17 08:54:42 +0100

    s3form

---
+++ rebuild.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+cd "$(dirname "$0")"
+
+git pull --all > rebuild.log 2>&1
+git merge master --no-edit >> rebuild.log 2>&1
+DATE=`date +%Y-%m-%d`
+git commit --allow-empty -m "$DATE: check for new Zotero/Juris-M versions" >> rebuild.log 2>&1
+git push >> rebuild.log 2>&1
+
+/home/emile/.rvm/wrappers/ruby-2.3.0@zotero/rake s3form
