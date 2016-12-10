394e505aebe46ad544e8321be654ce432226953f
retorquere <Emiliano.Heyns@iris-advies.com>
2016-12-10 10:05:51 +0100

    forgot to redirect to log

--- rebuild.sh
+++ rebuild.sh
@@ -6,12 +6,12 @@ cd "$(dirname "$0")"
 
 . .env
 
-git checkout master > rebuild.log 2>&1
-git pull >> rebuild.log 2>&1
-git checkout nightly
-git merge master --no-edit >> rebuild.log 2>&1
+git checkout master &> rebuild.log
+git pull &>> rebuild.log
+git checkout nightly &>> rebuild.log
+git merge master --no-edit &>> rebuild.log
 DATE=`date +%Y-%m-%d`
-git commit --allow-empty -m "$DATE: check for new Zotero/Juris-M versions" >> rebuild.log 2>&1
-git push >> rebuild.log 2>&1
+git commit --allow-empty -m "$DATE: check for new Zotero/Juris-M versions" &>> rebuild.log
+git push &>> rebuild.log
 
 /home/emile/.rvm/wrappers/ruby-2.3.0@zotero/rake s3form
